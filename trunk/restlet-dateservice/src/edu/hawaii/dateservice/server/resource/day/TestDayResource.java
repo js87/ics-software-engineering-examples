74062de14da05dbacdd80361d623e40303f90da1
philipmjohnson <philipmjohnson@324bf08f-eef9-c022-f4a2-12dfa1e551f6>
2011-01-08 04:54:27 +0000

    Initial commit of working version of dateservice.

---
+++ trunk/restlet-dateservice/src/edu/hawaii/dateservice/server/resource/day/TestDayResource.java
@@ -0,0 +1,42 @@
+package edu.hawaii.dateservice.server.resource.day;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Calendar;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.restlet.resource.ClientResource;
+import edu.hawaii.dateservice.server.DateServer;
+
+/**
+ * Tests the operations supported for the DateServer day resource.
+ * @author Philip Johnson
+ */
+public class TestDayResource {
+
+  /** The port used for testing. */
+  private static int testPort = 8112;
+
+  /**
+   * Start up a test server before testing any of the operations on this resource.
+   * @throws Exception If problems occur starting up the server. 
+   */
+  @BeforeClass
+  public static void startServer () throws Exception {
+    DateServer.runServer(testPort);
+  }
+  
+  /**
+   * Test the GET method of the day resource.
+   * @throws Exception If problems occur. 
+   */
+  @Test
+  public void testGet() throws Exception {
+    // Try to retrieve the year from the DateServer.
+    String testUrl = String.format("http://localhost:%s/dateserver/day", testPort);
+    ClientResource client = new ClientResource(testUrl);
+    String response = client.get().getText();
+    // Now check to see if we got the right response. 
+    String currDay = String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+    assertEquals("Testing day", currDay, response);
+  }
+}
---
+++ trunk/restlet-dateservice/src/edu/hawaii/dateservice/server/resource/year/YearResource.java
@@ -0,0 +1,25 @@
+package edu.hawaii.dateservice.server.resource.year;
+
+import java.util.Calendar;
+import java.util.Locale;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+/**
+ * A server resource that will handle requests for the current year.
+ * A new instance of this resource is created for each request. 
+ * @author Philip Johnson
+ */
+public class YearResource extends ServerResource {
+  
+  /**
+   * Returns the year as of the time this request was received. 
+   * @return The year as a text string. 
+   */
+  @Get
+  public String getYear() {
+    Calendar currentDate = Calendar.getInstance(Locale.US);
+    return String.valueOf(currentDate.get(Calendar.YEAR));
+  }
+
+}
