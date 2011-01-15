6b91763525e94d7defc35c796409e251fc5960d8
philipmjohnson <philipmjohnson@324bf08f-eef9-c022-f4a2-12dfa1e551f6>
2011-01-15 07:26:32 +0000

    The working system.

--- trunk/restlet-contactservice/src/edu/hawaii/contactservice/server/db/Contacts.java
+++ trunk/restlet-contactservice/src/edu/hawaii/contactservice/server/db/Contacts.java
@@ -1,18 +1,19 @@
 package edu.hawaii.contactservice.server.db;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import edu.hawaii.contactservice.common.contact.Contact;
 
 /**
  * Holds an in-memory representation of all Contacts present in this service. 
  * There is no persistency, so the Contacts disappear when this service ceases execution.
+ * This collection needs to be thread-safe. 
  * @author Philip Johnson
  */
-public class Contacts implements Iterable<Contact> {
+public class Contacts {
   /** Holds a map from unique IDs to the corresponding Contact instance. */
-  private Map<String, Contact> uniqueID2Contact = new HashMap<String, Contact>();
+  private Map<String, Contact> uniqueID2Contact = new ConcurrentHashMap<String, Contact>();
   
   /** Create the singleton instance of this Contacts at class definition time. */
   private static Contacts theInstance = new Contacts();
@@ -35,7 +36,7 @@ public class Contacts implements Iterable<Contact> {
    * Note that if a contact with the same unique ID is already present, it is overridden. 
    * @param contact The contact to be added.
    */
-  public void addContact(Contact contact) {
+  public synchronized void addContact(Contact contact) {
     uniqueID2Contact.put(contact.getUniqueID(), contact);
   }
   
@@ -45,29 +46,24 @@ public class Contacts implements Iterable<Contact> {
    * @param uniqueID The uniqueID to search for. 
    * @return The Contact instance or null.
    */
-  public Contact getContact(String uniqueID) {
+  public synchronized Contact getContact(String uniqueID) {
     return uniqueID2Contact.get(uniqueID);
+  }
+  
+  /**
+   * Ensures the Contact represented by uniqueID is no longer in the database. 
+   * @param uniqueID The uniqueID to remove, if present. 
+   */
+  public synchronized void deleteContact(String uniqueID) {
+    uniqueID2Contact.remove(uniqueID);
   }
 
 
   /**
-   * Return an iterator over the Contact instances in this collection. This enables the 
-   * use of Contacts in for-each loops.
+   * Return a snapshot of the collection at the point in time that this was called.
+   * @return A collection of Contact instances.  
    */
-  @Override
-  public Iterator<Contact> iterator() {
-    return uniqueID2Contact.values().iterator();
+  public Collection<Contact> getContacts () {
+    return uniqueID2Contact.values();
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
 }
--- trunk/restlet-contactservice/src/edu/hawaii/contactservice/server/resource/contact/ContactResource.java
+++ trunk/restlet-contactservice/src/edu/hawaii/contactservice/server/resource/contact/ContactResource.java
@@ -1,7 +1,14 @@
 package edu.hawaii.contactservice.server.resource.contact;
 
+import org.restlet.data.Status;
+import org.restlet.ext.xml.DomRepresentation;
+import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Put;
+import org.restlet.resource.Delete;
 import org.restlet.resource.ServerResource;
+import edu.hawaii.contactservice.common.contact.Contact;
+import edu.hawaii.contactservice.server.db.Contacts;
 
 /**
  * A server resource that will handle requests regarding a specific Contact.
@@ -12,15 +19,54 @@ import org.restlet.resource.ServerResource;
 public class ContactResource extends ServerResource {
   
   /**
-   * Returns the Contact instance requested given its unique ID. 
-   * @return The 
+   * Returns the Contact instance requested by the URL. 
+   * @return The XML representation of the contact, or CLIENT_ERROR_NOT_FOUND if the 
+   * unique ID is not present.
+   * @throws Exception If problems occur making the representation. Shouldn't occur in 
+   * practice but if it does, Restlet will set the Status code. 
    */
   @Get
-  public String getContact() {
+  public Representation getContact() throws Exception {
+    DomRepresentation result = new DomRepresentation();
+    // Get the requested Contact ID from the URL.
     String uniqueID = (String)this.getRequestAttributes().get("uniqueID");
-    
-
+    // Look for it in the Contacts database.
+    Contact contact = Contacts.getInstance().getContact(uniqueID);
+    if (contact == null) {
+      // It was not found, so set the status to indicate this.
+      getResponse().setStatus(Status.CLIENT_ERROR_NOT_ACCEPTABLE);
+    } 
+    else {
+      // It was found, so add the Contact's XML representation to the response.
+      result.setDocument(contact.toXml());
+      }
+    return result;
+  }
+  
+  /**
+   * Adds the passed Contact to our internal database of Contacts.
+   * @param representation The XML representation of the new Contact to add.
+   * @return null.
+   * @throws Exception If problems occur unpacking the representation.
+   */
+  @Put
+  public Representation putContact(Representation representation) throws Exception {
+    DomRepresentation domRepresentation = new DomRepresentation(representation);
+    Contact contact = new Contact(domRepresentation.getDocument());
+    Contacts.getInstance().addContact(contact);
     return null;
   }
-
+  
+  /**
+   * Deletes the unique ID from the internal database. 
+   * @return null.
+   */
+  @Delete
+  public Representation deleteContact() {
+    // Get the requested Contact ID from the URL.
+    String uniqueID = (String)this.getRequestAttributes().get("uniqueID");
+    // Make sure it is no longer present in the Contacts database. 
+    Contacts.getInstance().deleteContact(uniqueID);
+    return null;
+  }
 }
